a4439e3831d16bb711dfa70669ce94325741a088
Chaudhari-Ankita <chaudhariankita1410@gmail.com>
2024-11-08 16:57:26 -0500

    index.html changes added

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	fs := http.FileServer(http.Dir("./templates")) // created a variable 'fs' that points to the "./templates" directory.
+	http.Handle("/", fs)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
